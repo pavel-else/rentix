f3eb4e2339038d8bc63276b3968623339a551a23
pavel-else <de.pavel.se@yandex.ru>
2019-02-08 12:11:20 +0300

    Build - added new block info

--- Rentix_wrap/app_config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    $address_site = "https://rentix.biz";
-
-    // Расположение приложения точки проката
-    //
-    // $rental_host = "https://app.rentix.biz/#/";
-    $rental_host = "http://localhost:8080/#/"; 
