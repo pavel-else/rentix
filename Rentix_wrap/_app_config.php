f2b9c2ae9675cde07698d4c1bab6abaa9ead69aa
pavel-else <de.pavel.se@yandex.ru>
2019-04-17 16:24:29 +0300

    Refactoring

---
+++ Rentix_wrap/_app_config.php
@@ -0,0 +1,8 @@
+<?php 
+    // Real
+    $address_site = "https://rentix.biz";
+    $rental_host = "https://app.rentix.biz/#/";
+
+    //  // Test
+    // $address_site = "https://test.rentix.biz";
+    // $rental_host = "http://localhost:8080/#/"; 
--- Rentix_wrap/libs/user_uploads.php
+++ Rentix_wrap/libs/user_uploads.php
@@ -1,8 +1,11 @@
 <?php
     error_reporting(E_ALL & ~E_NOTICE);
     date_default_timezone_set('Europe/Moscow');
-
+    header('Content-Type: application/json');
     header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    header('Access-Control-Allow-Credentials: true');
 
     $checkType = function ($type) {
         switch ($type) {
