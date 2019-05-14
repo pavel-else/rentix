b8ab875afd6cb7b4dcd51bd272ba390a646aaef7
pavel-else <de.pavel.se@yandex.ru>
2019-05-14 07:37:43 +0300

    commit

--- Rentix_wrap/_app_config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    // Real
-    $address_site = "https://rentix.biz";
-    $rental_host = "https://app.rentix.biz/#/";
-
-    //  // Test
-    // $address_site = "https://test.rentix.biz";
-    // $rental_host = "http://localhost:8080/#/"; 
