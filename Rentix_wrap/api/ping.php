3d7037a31411ea154e319240e32dadbb6974b782
pavel-else <de.pavel.se@yandex.ru>
2019-05-22 12:45:11 +0300

    1

--- Rentix_wrap/api/ping.php
+++ Rentix_wrap/api/ping.php
@@ -1,20 +1,3 @@
 <?php
 
-error_reporting(E_ALL & ~E_NOTICE);
-date_default_timezone_set('Europe/Moscow');
-
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
-header('Access-Control-Allow-Credentials: true');
-
-
-$postDataJSON = file_get_contents('php://input');
-$dataJSON = json_decode($postDataJSON, true);
-
-$data = [
-    'response' => 'OK'
-];
-
-echo json_encode($data);
+echo 1;
